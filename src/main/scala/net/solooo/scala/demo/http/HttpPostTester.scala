f45f3f20c02bb2ac289ab810d2254fdb3e36e950
peij <peij@htdatacloud.com>
2017-01-09 10:58:56 +0800

    访问url并获取返回值，添加请求超时时间

--- src/main/scala/net/solooo/scala/demo/http/HttpPostTester.scala
+++ src/main/scala/net/solooo/scala/demo/http/HttpPostTester.scala
@@ -13,8 +13,25 @@ package net.solooo.scala.demo.http
 object HttpPostTester {
 
   def main(args: Array[String]) {
-    val url = "http://192.168.1.82:8083/gms/auth/login?userName=admin&password=123456"
-    val result = scala.io.Source.fromURL(url, "utf-8").mkString
-    println(result)
+//    val url = "http://192.168.1.82:8083/gms/auth/login?userName=admin&password=123456"
+//    val result = scala.io.Source.fromURL(url, "utf-8").mkString
+//    println(result)
+    val str = fromUrlWithTimeout("http://192.168.1.82:8083/gms/auth/login?userName=admin&password=123456", 3000)
+    println(str)
+  }
+
+  def fromUrlWithTimeout(url: String, timeout: Int = 1500): String = {
+    import java.net.URL
+    import scala.io.Source
+    val conn = (new URL(url)).openConnection()
+    conn.setConnectTimeout(timeout)
+    conn.setReadTimeout(timeout)
+    val stream = conn.getInputStream()
+    val src = (scala.util.control.Exception.catching(classOf[Throwable]) opt Source.fromInputStream(stream).mkString) match {
+      case Some(s: String) => s
+      case _ => ""
+    }
+    stream.close()
+    src
   }
 }
